cf3b447827da472d19a91a78c7e312608ff5710e
Diego Lopes <114710740+llopesdiego@users.noreply.github.com>
2023-08-19 00:59:04 -0300

    aula 08 e 11

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 78
+console.log(`A Velocidade do seu carro é ${vel}km/h`)
+if (vel >60){
+    console.log('Você ultrapassou a velocidade permitida. Multado!')
+}
+
+console.log('Dirija sempre usando cinto de segurança!')
