0179cacccdc0e90f4181cd172c1ad25b046ba70a
Diego Lopes <114710740+llopesdiego@users.noreply.github.com>
2023-08-20 00:55:39 -0300

    ATUALIZACAO

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,6 +1,10 @@
-console.log()
+console.log('Começa Aqui')
+for ( var c = 1 ; c <=6; c++){
+    console.log(`Passo ${c}`)
+}
+console.log('Termina Aqui')
 
-/*var c = 1
+/*var c = 1 
 do{
     console.log(`Passo ${c}`)
     c++    
---
+++ aula15/ambiente.js
@@ -0,0 +1,5 @@
+let num = [5 , 8 , 3 , 9, 2]
+
+console.log(`Nosso vetor é ${num}`)
+num.sort()
+console.log(`Ordenado fica ${num}`)
